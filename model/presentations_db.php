30a7b3ddfc6737a4e446e02fb6732ee24cbe69a7
bryres <bryres@bergen.org>
2015-12-14 09:51:07 -0500

    Database connection setup

---
+++ model/presentations_db.php
@@ -0,0 +1,51 @@
+<?php
+function get_list ($query) {
+    global $db;
+
+    try {
+        $statement = $db->prepare($query);  
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $statement->closeCursor();
+        return $result;
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        display_db_error($error_message);
+    }    
+}
+
+function get_session_times() {
+    $query = 	'SELECT ses_id, ses_name, ses_start, ses_end 
+					from session_times
+					order by sort_order';
+					
+    return get_list($query);
+}
+
+function get_presentation_list($ses_id) {
+    $query = 	'SELECT mentor.mentor_id,  mentor_last_name ,  mentor_first_name ,  mentor_field ,
+					mentor_position , mentor_company ,  mentor_profile ,  mentor_keywords ,  
+					mentor_email ,  mentor_cell_nbr , mentor_phone_nbr ,  mentor_address ,  
+					mentor_source ,  mentor_notes ,  active ,  pres_room , pres_host_teacher ,  
+					pres_max_capacity , presentation.pres_enrolled_count
+				FROM mentor
+				INNER JOIN presentation ON presentation.mentor_id = mentor.mentor_id
+				WHERE presentation.ses_id = :ses_id
+				AND mentor.active =1
+				ORDER BY mentor_last_name, mentor_first_name';
+	
+    global $db;
+
+    try {
+        $statement = $db->prepare($query);  
+        $statement->bindValue(':ses_id', $ses_id);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $statement->closeCursor();
+        return $result;
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        display_db_error($error_message);
+    }  
+}
+?>
