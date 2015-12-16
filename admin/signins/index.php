181700687f7d3de911698d46f0fcfde027bf70b1
David <davidcyyi@gmail.com>
2015-12-16 10:03:00 -0500

    included cases for action and needed variable

--- admin/signins/index.php
+++ admin/signins/index.php
@@ -5,17 +5,27 @@
  * Date: 12/16/15
  * Time: 9:04 AM
  */
+require_once("../util/main.php");
 require_once("../admin/signins/sheetRequest");
-require_once('tcpdf/config/lang/eng.php');
-require_once('tcpdf/tcpdf.php');
-$action = filter_input(INPUT_POST, 'action');
+require_once('../tcpdf/config/lang/eng.php');
+require_once('../tcpdf/tcpdf.php');
+$action =  strtolower(filter_input(INPUT_POST, 'action'));
 
+if ($action == NULL) {
+    $action = strtolower(filter_input(INPUT_GET, 'action'));
+    if ($action == NULL) {
+        $action = 'mentors';
+    }
+}
 switch ($action){
     case 'mentors': //presenter
-
+        $error_msg = '';
         break;
 
-    case 'class':
+    case 'session':
+        $error_msg = '';
+        $session = filter_input(INPUT_POST, 'session');
+
         break;
 }
 
