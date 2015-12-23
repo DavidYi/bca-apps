971a81e49c71b967a82cddea570b6df6e23518fe
ekyu <Emily@Macintosh.local>
2015-12-23 09:01:02 -0500

    adding index

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,27 @@
 <?php
 include('../util/main.php');
-
-if (!isset($_SESSION))
-    session_start();
+$action = strtolower(filter_input(INPUT_POST, 'action'));
+if ($action == NULL) {
+    $action = strtolower(filter_input(INPUT_GET, 'action'));
+    if ($action == NULL) {
+        $action = 'list_options';
+    }
+}
 
 // add some kind of check here to make sure the user is logged in as an admin
 // util/main checks for logged in, don't know how to check for admin permissions
 
+switch($action) {
+    case 'list_options':
+        break;
+    case 'auto_enroll':
+        //code here
+        break;
+    default:
+        display_error('Unknown account action: ' . $action);
+        exit();
+        break;
+}
 ?>
 
 <html>
@@ -18,6 +33,6 @@ if (!isset($_SESSION))
 <h1>Admin Tools</h1>
 <p><a href = "signins/index.php">Signins</a><br>
 <a href = "signup_dates/index.php">Signup Dates</a><br>
-    <a href = "signup_dates/index.php">Signup Dates</a></p>
+<a href = "signup_status/index.php">Signup Status</a></p>
 </body>
 </html>
