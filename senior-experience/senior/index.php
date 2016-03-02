38b055d618c868283edf15bb76f2191d0deebdcf
LaScalaLuke <lascala.luke@gmail.com>
2016-03-02 09:40:01 -0500

    Made redirect when a senior signs in and is eligible to make a presentation. Added senior DB.

--- senior-experience/senior/index.php
+++ senior-experience/senior/index.php
@@ -4,18 +4,15 @@ require_once('../util/main.php');
 //require_once('../../util/tags.php');
 require('../model/senior_db.php');
 
-if($user->usr_grade_lvl != 12){
-    echo($user->usr_grade_lvl);
+if(!isSeniortime()){
     header("Location: ../itinerary");
 }
 
 $action = strtolower(filter_input(INPUT_POST, 'action'));
 if ($action == NULL) {
     $action = strtolower(filter_input(INPUT_GET, 'action'));
-    if ($action == NULL) {
-        $action = 'list_presentations';
-    }
 }
+
 
 switch ($action) {
     case 'show_add_pres':
@@ -31,12 +28,14 @@ switch ($action) {
 
         add_pres($pres_title, $pres_desc, $pres_organization, $pres_location, $pres_names);
 
+
+        include 'show_pres.php';
+
         break;
 
 
-
-    default :
-        include 'list_pres.php';
+    default:
+        include 'show_pres.php';
 
 
 }
