8447edf10c26c3be0eac8d5ad0cd1aa7e3a03b3c
bryres <bryres@bergen.org>
2016-03-17 12:28:43 -0400

    log changes

--- career-day/model/presentations_db.php
+++ career-day/model/presentations_db.php
@@ -30,9 +30,8 @@ function get_session_times_by_id($ses_id) {
 
 function get_presentation_list($ses_id, $sort_by, $order_by) {
     $query = 	'SELECT mentor.mentor_id,  mentor_last_name ,  mentor_first_name ,  mentor_field ,
-					mentor_position , mentor_company ,  mentor_profile ,  mentor_keywords ,  
-					mentor_email ,  mentor_cell_nbr , mentor_phone_nbr ,  mentor_address ,  
-					mentor_source ,  mentor_notes ,  active ,  pres_room , pres_host_teacher ,  
+					mentor_position , mentor_company ,  mentor_profile ,  mentor_keywords ,
+					active ,  pres_room , pres_host_teacher ,
 					pres_max_capacity , presentation.pres_enrolled_count, presentation.pres_id, 
 					pres_max_capacity - presentation.pres_enrolled_count as remaining
 				FROM mentor
