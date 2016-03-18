55e594d20f2ac2c2e461c639b0a2770e02d81c48
Kincent Lan <kinlan19@bergen.org>
2016-03-18 11:35:21 -0400

    Created Log Page.

--- career-day/admin/log_viewer/view.php
+++ career-day/admin/log_viewer/view.php
@@ -1,6 +1,42 @@
 <html>
 
-<?php echo sizeof($logs); ?>
+<?php sizeof($logs); ?>
 
+<link rel="stylesheet" type="text/css" href="../css/mentor_list.css">
+
+<html>
+
+<section>
+    <h1>Log Viewer</h1>
+    <table class="gridtable">
+
+
+        <tr>
+            <th>Date/Time </th>
+            <th>Lvl. </th>
+            <th>Name </th>
+            <th>Message </th>
+
+        </tr>
+
+        <?php foreach ($logs as $log) :
+        // Get product data
+        $logDate = $log['log_dt'];
+        $logLvl = $log['log_lvl_cde'];
+        $logMsg = $log['log_msg'];
+        $logName = $log['name'];
+
+        ?>
+        <tr>
+            <td nowrap><?php echo $logDate; ?></td>
+            <td nowrap><?php echo $logLvl; ?></td>
+            <td nowrap><?php echo $logName; ?></td>
+            <td nowrap><?php echo $logMsg; ?></td>
+
+
+
+            <?php endforeach; ?>
+    </table>
+</section>
 
 </html>
