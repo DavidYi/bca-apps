a9f6ebd41ab1372be5d176f2f357a83cd1189aa3
Dennis Mikhaylov <dennismikhaylov@gmail.com>
2016-03-23 09:33:49 -0400

    testing timedatepicker

--- career-day/admin/signup_dates/test.php
+++ career-day/admin/signup_dates/test.php
@@ -3,13 +3,13 @@
 <head>
 
     <meta charset="utf-8">
-    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-    <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
-    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-    <script src="http://trentrichardson.com/examples/timepicker/jquery-ui-timepicker-addon.js"></script>
-    <link rel="stylesheet" href="http://trentrichardson.com/examples/timepicker/jquery-ui-timepicker-addon.css">
+    <script src="DateTimePicker/jquery-1.9.1.min.js"></script>
+    <script src="DateTimePicker/jquery-migrate-1.2.1.min.js"></script>
+    <link rel="stylesheet" href="DateTimePicker/jquery-ui.css">
+    <script src="DateTimePicker/jquery-1.9.1.js"></script>
+    <script src="DateTimePicker/jquery-ui.js"></script>
+    <script src="DateTimePicker/jquery-ui-timepicker-addon.js"></script>
+    <link rel="stylesheet" href="DateTimePicker/jquery-ui-timepicker-addon.css">
 
     <script>
         $(function() {
