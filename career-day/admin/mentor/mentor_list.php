7af15b30d6e4f7f197f914f659e26c0ce2014516
Kincent Lan <kinlan19@bergen.org>
2016-03-23 10:47:12 -0400

    Dennis halp

--- career-day/admin/mentor/mentor_list.php
+++ career-day/admin/mentor/mentor_list.php
@@ -1,89 +1,85 @@
 <script type="text/javascript">
-        function deleteMentor(mentorID)
-        {
-            if (confirm('Are you sure you would like to delete the mentor?'))
-            {
-                window.parent.parent.location.href = 'view.php?action=delete_mentor&mentor_id=' + mentorID;
-            }
+    function deleteMentor(mentorID) {
+        if (confirm('Are you sure you would like to delete the mentor?')) {
+            window.parent.parent.location.href = 'view.php?action=delete_mentor&mentor_id=' + mentorID;
         }
+    }
 
-    </script>
+</script>
+<head>
+    <title>Admin: Mentor</title>
 
-<link rel="stylesheet" type="text/css" href="../css/mentor_list.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 
-    <section>
-        <h1>Mentor List</h1>
-        <table class="gridtable">
+    <!-- Styles -->
+    <link href="../../admin/ss/main.css" rel="stylesheet">
+</head>
 
-            <a href="index.php?action=show_add_mentor">Add New Mentor</a>
-            <tr class="tablerow">
-                <th>Last Name </th>
-                <th>First Name </th>
-                <th>Company</th>
-                <th>Room</th>
-                <th>Position</th>
-                <th>Host Teacher</th>
-                <th>Max Capacity</th>
+<body>
+<header>
+    <h1 class="title">Mentors</h1>
+</header>
 
-
-
-            </tr>
-
-            <?php foreach ($mentorList as $mentor) :
-
-                $mentorId = $mentor['mentor_id'];
-                $mentor_last_name = $mentor['mentor_last_name'];
-                $mentor_first_name = $mentor['mentor_first_name'];
-                $mentor_position = $mentor['mentor_position'];
-                $mentor_company = $mentor['mentor_company'];
-                $pres_room = $mentor['pres_room'];
-                $pres_host_teacher = $mentor['pres_host_teacher'];
-                $pres_max_capacity = $mentor['pres_max_capacity'];
-
-                ?>
-
-                <tr class="clicky" onclick="modify(<?php echo($mentorId)?>);">
-
-
-                    <td nowrap>
-                        <?php echo $mentor_last_name; ?>
-                    </td>
-
-                    <td nowrap>
-                        <?php echo $mentor_first_name; ?>
-                    </td>
-
-                    <td>
-                        <?php echo $mentor_company; ?>
-                    </td>
-
-                    <td >
-                        <?php echo $pres_room; ?>
-                    </td>
-
-                    <td >
-                        <?php echo $mentor_position; ?>
-                    </td>
-                    <td >
-                        <?php echo $pres_host_teacher; ?>
-                    </td>
-
-                    <td >
-                        <?php echo $pres_max_capacity; ?>
-                    </td>
+<nav class="navbar">
+    <a href="#">
+        <div class="session-filter last_name">Last Name</div>
+    </a>
+    <a href="#">
+        <div class="session-filter first_name">First Name</div>
+    </a>
+    <a href="#">
+        <div class="session-filter company">Company</div>
+    </a>
+    <a href="#">
+        <div class="session-filter position">Room</div>
+    </a>
+    <a href="#">
+        <div class="session-filter teacher">Position</div>
+    </a>
+    <a href="#>">
+        <div class="session-filter room">Teacher</div>
+    </a>
+    <a href="#">
+        <div class="session-filter capacity">Max</div>
+    </a>
+</nav>
 
 
+<?php foreach ($mentorList as $mentor) :
 
+$mentorId = $mentor['mentor_id'];
+$mentor_last_name = $mentor['mentor_last_name'];
+$mentor_first_name = $mentor['mentor_first_name'];
+$mentor_position = $mentor['mentor_position'];
+$mentor_company = $mentor['mentor_company'];
+$pres_room = $mentor['pres_room'];
+$pres_host_teacher = $mentor['pres_host_teacher'];
+$pres_max_capacity = $mentor['pres_max_capacity'];
 
+?>
 
+<div class="list-container" onclick="modify(<?php echo($mentorId) ?>);">
+    <div class="mentor">
 
+        <div class="session-filter last_name"><?php echo $mentor_last_name; ?></div>
+        <div class="session-filter first_name"><?php echo $mentor_first_name; ?></div>
+        <div class="session-filter company"><?php echo $mentor_company; ?></div>
+        <div class="session-filter room"><?php echo $pres_room; ?></div>
+        <div class="session-filter position"><?php echo $mentor_position; ?></div>
+        <div class="session-filter teacher"><?php echo $pres_host_teacher; ?></div>
+        <div class="session-filter capacity"><?php echo $pres_max_capacity; ?></div>
 
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    </section>
+        <?php endforeach; ?>
+    </div>
+</div>
 
-<script>function modify($mentorId){
-        window.parent.parent.location.href="index.php?action=show_modify_mentor&mentor_id=" + $mentorId;
+<div class="fab">
+    <a id="fab-action" trigger="mentor-add.html"><span class="plus">+</span></a>
+</div>
+
+</body>
+
+<script>function modify($mentorId) {
+        window.parent.parent.location.href = "index.php?action=show_modify_mentor&mentor_id=" + $mentorId;
     }
 </script>
