0cf60432ac6c9f810c00748f9fae68a1352ebca5
jbarbella <juliann.barbella@gmail.com>
2016-03-30 10:25:59 -0400

    Working on the landing page for users (proctoring)

--- teacher-proctoring/model/database.php
+++ teacher-proctoring/model/database.php
@@ -3,9 +3,9 @@
 #
 # Application specific database connection
 #
-$dsn = 'mysql:host=webdev01.bergen.org;dbname=atcsdevb_career_day';
-$username = 'atcsdevb_carusr';
-$password = 'W[6V8tgSX=y~';
+$dsn = 'mysql:host=webdev01.bergen.org;dbname=atcsdevb_teacher_dashboard';
+$username = 'atcsdevb_tchrusr';
+$password = '^D3cg)*?i2g#';
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 $debug_mode = true;
