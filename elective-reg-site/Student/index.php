1b95c3c8f979007e1a32ecce6c370faf67ee18b8
joshClune <josh.seth.clune@gmail.com>
2016-04-20 10:12:11 -0400

    Modifying index more

--- elective-reg-site/Student/index.php
+++ elective-reg-site/Student/index.php
@@ -1,13 +1,11 @@
 <?php
-<<<<<<< HEAD
+
 /**
  * Created by PhpStorm.
  * User: joshClune
  * Date: 4/6/16
  * Time: 10:36 AM
  */
-echo "This is the index.php in Student"
-=======
 include('../util/main.php');
 
 $action = strtolower(filter_input(INPUT_POST, 'action'));
@@ -38,5 +36,4 @@ switch ($action) {
         exit();
         break;
 }
->>>>>>> 0df83cfd13f2c74bff6cd6204d2803ed3fba473a
 ?>
