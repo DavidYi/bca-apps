4ee93fa2d9267a1b9c419898df9f16b14032bf51
joshClune <josh.seth.clune@gmail.com>
2016-04-20 10:29:40 -0400

    Modifying index more

--- elective-reg-site/Student/view/view.php
+++ elective-reg-site/Student/view/view.php
@@ -9,6 +9,8 @@
     <!--Above file doesn't exist yet, copy it from career day later-->
 </head>
 <body>
+<section class="main view">
+    <div class="view-main">
 <main>
     <header><h1 class="title"><h2>Student Side</h1>
         <div id="logout"><a href="./index.php?action=logout">Log Out</a></div>
@@ -24,6 +26,27 @@
 
     <!-- should probably be /index.php?action=logout in the final, but that won't work right on localhost since everything's in bca-apps rn -->
 </main>
+    </div>
+
+    <div class="view-signup enrollment">
+    <div class="vertical-center">
+        <main>
+            <header><h1 class="title"><h2>Student Side</h1>
+                <div id="logout"><a href="./index.php?action=logout">Log Out</a></div>
+            </header>
+            <br>
+            <div class="feature">
+                <h3>When I'm available: <a href="./index.php?action=modify_times">Modify</a></h3>
+                <p>There will be a list of available times here</p></div>
+            <div class="feature">
+                <h3>I want to take: <a href="./index.php?action=modify_courses">Modify</a></h3>
+                <p>There will be a list of courses here</p></div>
+
+
+            <!-- should probably be /index.php?action=logout in the final, but that won't work right on localhost since everything's in bca-apps rn -->
+        </main>
+    </div>
+    </div>
+</section>
 </body>
 </html>
-
