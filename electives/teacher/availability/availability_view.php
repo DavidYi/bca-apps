a0d57e6e1c7dbc2ac2d4788982b3c894eb48483e
Iris <iris.val19@gmail.com>
2016-04-22 01:02:06 -0400

    Almost finished styles

--- electives/teacher/availability/availability_view.php
+++ electives/teacher/availability/availability_view.php
@@ -1,92 +1,97 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Table Style</title>
+	<meta name="viewport" content="initial-scale=1.0; maximum-scale=1.0; width=device-width;">
+  <link rel="stylesheet" href="index.css">
+</head>
 
- <!-- * Created by PhpStorm.
- * User: iris !!
- * Date: 3/28/16
- * Time: 9:42 PM -->
+<body>
+<div class="table-title">
+<h3>Schedule Preferences</h3>
+</div>
 
-<!-- currently is spaghetti code -->
-  <!DOCTYPE html>
-  <html>
-  <head>
-      <meta charset="utf-8">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="index.css">
-      <title>Schedule Preferences</title>
-  </head>
+<table class="table-fill">
+<thead>
+<tr>
+  <th class="text-left">Monday</th>
+  <th class="text-left">Tuesday</th>
+  <th class="text-left">Wednesday</th>
+  <th class="text-left">Thursday</th>
+  <th class="text-left">Friday</th>
+</tr>
+</thead>
+<tbody class="table-hover">
 
-  <script>
+<tr>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Monday 1-3">1-3</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Tuesday 1-3">1-3</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Wednesday 1-3">1-3</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Thursday 1-3">1-3</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Friday 1-3">1-3</label></td>
+</tr>
 
-  $( document ).ready(function() {
-    if(document.getElementsByClassName('mods').checked) {
-        $(this).parent().css("background-color", "yellow");
-    }
-  });
-
-  </script>
-
-  <body>
-    <h1 class="header">Preferable Elective Times:</h1>
-      <form id="admin_schedule" method="get" action="">
-        <div class="column">
-          <label><input type="checkbox" class="mods" name="time[]" value="Monday 1-3">1-3</label>
-          <label><input type="checkbox" class="mods" name="time[]" value="Monday 4-6">4-6</label>
-          <label><input type="checkbox" class="mods" name="time[]" value="Monday 7-9">7-9</label>
-          <label><input type="checkbox" class="mods" name="time[]" value="Monday 10-12">10-12</label>
-          <label><input type="checkbox" class="mods" name="time[]" value="Monday 13-15">13-15</label>
-          <label><input type="checkbox" class="mods" name="time[]" value="Monday 16-18">16-18</label>
-          <label><input type="checkbox" class="mods" name="time[]" value="Monday 19-21">19-21</label>
-          <label><input type="checkbox" class="mods" name="time[]" value="Monday 22-24">22-24</label>
-        </div>
-
-
-          <div class="column">
-            <label><input type="checkbox" class="mods" name="time[]" value="Tuesday 1-3">1-3</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Tuesday 4-6">4-6</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Tuesday 7-9">7-9</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Tuesday 10-12">10-12</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Tuesday 13-15">13-15</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Tuesday 16-18">16-18</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Tuesday 19-21">19-21</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Tuesday 22-24">22-24</label>
-          </div>
-
-          <div class="column">
-            <label><input type="checkbox" class="mods" name="time[]" value="Wednesday 1-3">1-3</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Wednesday 4-6">4-6</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Wednesday 7-9">7-9</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Wednesday 10-12">10-12</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Wednesday 13-15">13-15</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Wednesday 16-18">16-18</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Wednesday 19-21">19-21</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Wednesday 22-24">22-24</label>
-          </div>
-
-          <div class="column">
-            <label><input type="checkbox" class="mods" name="time[]" value="Thursday 1-3">1-3</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Thursday 4-6">4-6</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Thursday 7-9">7-9</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Thursday 10-12">10-12</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Thursday 13-15">13-15</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Thursday 16-18">16-18</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Thursday 19-21">19-21</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Thursday 22-24">22-24</label>
-          </div>
-
-          <div class="column">
-            <label><input type="checkbox" class="mods" name="time[]" value="Friday 1-3">1-3</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Friday 4-6">4-6</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Friday 7-9">7-9</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Friday 10-12">10-12</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Friday 13-15">13-15</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Friday 16-18">16-18</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Friday 19-21">19-21</label>
-            <label><input type="checkbox" class="mods" name="time[]" value="Friday 22-24">22-24</label>
-          </div>
-
-            <input type="submit" value="submit">
+<tr>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Monday 1-3">4-6</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Tuesday 1-3">4-6</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Wednesday 4-6">4-6</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Thursday 4-6">4-6</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Friday 4-6">4-6</label></td>
+</tr>
 
 
-      </form>
+<tr>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Monday 7-9">7-9</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Tuesday 7-9">7-9</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Wednesday 7-9">7-9</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Thursday 7-9">7-9</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Friday 7-9">7-9</label></td>
+</tr>
+
+<tr>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Monday 10-12">10-12</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Tuesday 10-12">10-12</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Wednesday 10-12">10-12</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Thursday 10-12">10-12</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Friday 10-12">10-12</label></td>
+</tr>
+
+<tr>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Monday 13-15">13-15</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Tuesday 13-15">13-15</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Wednesday 13-15">13-15</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Thursday 13-15">13-15</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Friday 13-15">13-15</label></td>
+</tr>
+
+<tr>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Monday 16-18">16-18</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Tuesday 16-18">16-18</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Wednesday 16-18">16-18</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Thursday 16-18">16-18</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Friday 16-18">16-18</label></td>
+
+  </tr>
+
+ <tr>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Monday 19-21">19-21</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Tuesday 19-21">19-21</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Wednesday 19-21">19-21</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Thursday 19-21">19-21</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Friday 19-21">19-21</label></td>
+
+  </tr>
+
+  <tr>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Monday 22-24">22-24</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Tuesday 22-24">22-24</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Wednesday 22-24">22-24</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Thursday 22-24">22-24</label></td>
+  <td class="text-left"><label><input type="checkbox" class="mods" name="time[]" value="Friday 22-24">22-24</label></td>
+
+  </tr>
+</tbody>
+</table>
+
+
   </body>
-  </html>
