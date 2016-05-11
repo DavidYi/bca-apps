d32416a0b66d707b3509805b10baa98cda72a64f
Eric <ericpatel3000@gmail.com>
2016-05-11 09:22:26 -0400

    added admin page

--- senior-experience/admin/index.php
+++ senior-experience/admin/index.php
@@ -5,14 +5,15 @@ verify_admin();
 ?>
 
 <html>
-    <head>
-        <title>Admin Panel</title>
-    </head>
-    <body>
+<head>
+    <title>Admin Panel</title>
+</head>
+<body>
 <ul>
     <a href="field"><li>Field Panel</li></a>
     <a href="room"><li>Room Panel</li></a>
     <a href="presentation"><li>View Presentations</li></a>
+    <a href="signins"><li>Sign In</li></a>
 </ul>
 </body>
 </html>
