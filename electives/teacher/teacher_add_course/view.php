cfa87e91f1508cb5c52c78a6587233236f2b11de
Iris <iris.val19@gmail.com>
2016-05-18 08:57:43 -0400

    Offhours create course styling (not done)

--- electives/teacher/teacher_add_course/view.php
+++ electives/teacher/teacher_add_course/view.php
@@ -1,35 +1,33 @@
+<!doctype html>
 <html>
-<head>
-</head>
-<body>
-    <h1>Create a New Class</h1>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="view.css" rel="stylesheet" type="text/css" />
 
-    <form action="." method="post">
-        <input type="hidden" name="action" value="add_course">
-        <label>Class Name: </label><input name="class_name" type="text"> <br><br>
-        <label>Mods: </label><select name="mods">
-            <option value="1-3">1-3</option>
-            <option value="4-6">4-6</option>
-            <option value="7-9">7-9</option>
-            <option value="10-12">10-12</option>
-            <option value="13-15">13-15</option>
-            <option value="16-18">16-18</option>
-            <option value="19-21">19-21</option>
-            <option value="22-24">22-24</option>
-            <option value="25-27">25-27</option>
-        </select> <br><br>
-        <label>Day: </label><select name="day">
-            <option value="Monday">Monday</option>
-            <option value="Tuesday">Tuesday</option>
-            <option value="Wednesday">Wednesday</option>
-            <option value="Thursday">Thursday</option>
-            <option value="Friday">Friday</option>
-        </select> <br><br>
-        <label>Description: </label><br><br><textarea name="description"></textarea><br><br>
-        <input type="submit" name="choice" value="Add Course">
-        <input type="submit" name="choice" value="Cancel">
-    </form>
+  </head>
+  <body>
+    <form action="">
 
-</body>
+    <div id="box">
+      <p class="title">Create Course</p>
+
+        <label class="spacing">
+          <span>Course Name</span>
+          <input type="text" name="name">
+        </label>
+
+
+        <label class="spacing">
+            <span>Description</span>
+            <textarea name="textarea"></textarea>
+        </label>
+
+
+
+        <button class="submit" type="submit">Submit</button>
+      </form>
+  </div>
+  </body>
 </html>
-
