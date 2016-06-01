a004879bae26405e3e3ba0f91b026f3063750242
Iris <iris.val19@gmail.com>
2016-06-01 15:56:05 -0400

    Fixed styles for electives teacher preferences

--- electives/teacher/availability/availability_view.php
+++ electives/teacher/availability/availability_view.php
@@ -40,8 +40,8 @@
   </tbody>
   </table>
 <div class="wrapper">
-  <button name="action" value="submit" class="submit" type="submit">Submit</button>
-  <button name="action" value="back" class="submit" type="submit">Back</button>
+  <button name="action" value="submit" class="s submit" type="submit">Submit</button>
+  <button name="action" value="back" class="s back" type="submit">Back</button>
 </div>
 </form>
 </body>
