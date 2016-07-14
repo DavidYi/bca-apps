c6414dcaa20c2e15ea5191884fbd917f83c0af26
bryres <bryres@bergen.org>
2016-07-14 08:23:24 -0400

    added methods for updating the dates.

--- ida/model/signups_dates_db.php
+++ ida/model/signups_dates_db.php
@@ -20,9 +20,43 @@ function get_signup_dates_by_grade($grade_lvl) {
     }
 }
 
+//
+// This method provides no exception handling as the errors are handled in update_signup_dates.
+// Do not call this method directly.
+//
+function update_signup_date_for_grade ($grade_lvl, $start, $end)
+{
+    $query = 'update signup_dates       
+                set start = :start, end = :end
+                WHERE grade_lvl = :grade_lvl';
+
+    global $db;
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':grade_lvl', $grade_lvl);
+    $statement->bindValue(':start', $start);
+    $statement->bindValue(':end', $end);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
 function update_signup_dates ($start9, $end9, $start10, $end10, $start11, $end11, $start12, $end12)
 {
+    global $db;
+    $db->beginTransaction();
 
+    try {
+        update_signup_date_for_grade(9, $start9, $end9);
+        update_signup_date_for_grade(9, $start10, $end10);
+        update_signup_date_for_grade(9, $start11, $end11);
+        update_signup_date_for_grade(9, $start12, $end12);
+    }
+    catch (PDOException $e) {
+        $db->rollback();
+    } catch (PDOException $e) {
+        display_db_exception($e);
+        exit();
+    }
 }
 
 
