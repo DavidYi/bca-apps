0e65426b24e8db57c6342c67688c6bfadc8faa4f
CelinaPeralta <celper19@bergen.org>
2016-07-19 10:01:46 -0400

    teacher_status added

---
+++ teacher-proctoring/admin/status/view.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <link rel="stylesheet" type='text/css' href="styles.css">
+
+</head>
+
+<body>
+
+<div id="parents">
+
+    <table id="ts">
+        <tr id="fields">
+            <th>'Last'</th>
+            <th>'First'</th>
+            <th>'Hours'</th>
+        </tr>
+        <?php foreach ($teacher_status_list as $teacher) {
+
+
+        }
+        ?>
+    </table>
+
+
+</div>
+
+</body>
+</html>
