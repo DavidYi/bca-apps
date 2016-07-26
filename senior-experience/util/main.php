e1d269c8597e3ee84bc0d06f796706e8ae951ecf
Clink123 <jamie.lepone@gmail.com>
2016-07-26 11:28:26 -0400

    fixed logins

--- senior-experience/util/main.php
+++ senior-experience/util/main.php
@@ -5,6 +5,7 @@
 //
 $app_cde = 'SENX';
 $app_title = 'Senior Expositions';
+$shared_ss_url = '/bca-apps/shared/ss/main.css';
 
 $doc_root = filter_input(INPUT_SERVER, 'DOCUMENT_ROOT', FILTER_SANITIZE_STRING); // Looks like c:/xampp/htdocs
 
