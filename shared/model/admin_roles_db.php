50ef66776fa12690e93e408923d53f25f33af21a
Clink123 <jamie.lepone@gmail.com>
2016-07-26 09:15:26 -0400

    some things done

--- shared/model/admin_roles_db.php
+++ shared/model/admin_roles_db.php
@@ -5,14 +5,15 @@ function get_assigned_roles() {
 from role_application_user_xref x, user u, user_role r
 where x.usr_id = u.usr_id
 and x.usr_role_cde= r.usr_role_cde
-and app_cde = :app_code';
+and x.app_cde = r.app_cde
+and x.app_cde = :app_cde';
 
     global $db;
     global $app_cde;
 
     try {
         $statement = $db->prepare($query);
-        $statement->bindValue(':app_code', $app_cde);
+        $statement->bindValue(':app_cde', $app_cde);
         $statement->execute();
         $result = $statement->fetchAll();
         $statement->closeCursor();
@@ -46,12 +47,15 @@ order by usr_last_name, usr_first_name';
 function get_roles() {
     $query = 'select usr_role_desc, usr_role_cde
 from user_role
+where app_cde = :app_cde
 order by usr_role_desc';
 
     global $db;
+    global $app_cde;
 
     try {
         $statement = $db->prepare($query);
+        $statement->bindValue(':app_cde', $app_cde);
         $statement->execute();
         $result = $statement->fetchAll();
         $statement->closeCursor();
@@ -78,13 +82,16 @@ values (:usr_id, :app_cde, :usr_role_cde)';
 function delete_admin($usr_id, $usr_role_cde) {
     $query = 'delete from role_application_user_xref
                     where usr_role_cde = :usr_role_cde 
+                    and app_cde = :app_cde
                     and usr_id = :usr_id';
 
     global $db;
+    global $app_cde;
     try {
         $statement = $db->prepare($query);
         $statement->bindValue(":usr_role_cde", $usr_role_cde);
         $statement->bindValue(":usr_id", $usr_id);
+        $statement->bindValue(":app_cde", $app_cde);
         $statement->execute();
         $statement->closeCursor();
     } catch (PDOException $e) {
