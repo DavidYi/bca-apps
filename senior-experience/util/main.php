1a264c14e471852f148459bac19e83efa3e357c4
CelinaPeralta <celper19@bergen.org>
2016-07-28 09:43:06 -0400

    senior experience why

--- senior-experience/util/main.php
+++ senior-experience/util/main.php
@@ -6,11 +6,13 @@
 $app_cde = 'SENX';
 $app_title = 'Senior Expositions';
 
+include(__DIR__ . "/../../config.php");
+
 $shared_ss_url = '/' . $server_web_root . '/shared/ss/main.css';
 $app_url_path = $server_web_root . '/senior-experience';    // Name of the app on the web server.  Change this if the directory changes.
 
 // Provides environment specific configuration information.
-include(__DIR__ . "/../../config.php");
+
 
 
 /* This call back function is called once the user is authenticated.  This function handles redirecting the user
