9c9746eab93676df07dd433ecbfd78013ec19656
CelinaPeralta <celper19@bergen.org>
2016-08-02 08:47:07 -0400

    okay senx test login works now?

--- senior-experience/util/main.php
+++ senior-experience/util/main.php
@@ -1,28 +1,18 @@
 <?php
-//
-// Common imports that should be available on all pages.
-// Add them here.
-//
 $app_cde = 'SENX';
 $app_title = 'Senior Expositions';
 
+// Provides environment specific configuration information.
 include(__DIR__ . "/../../config.php");
 
 $shared_ss_url = '/' . $server_web_root . '/shared/ss/main.css';
 $app_url_path = $server_web_root . '/senior-experience';    // Name of the app on the web server.  Change this if the directory changes.
 
-// Provides environment specific configuration information.
-
-
-
 /* This call back function is called once the user is authenticated.  This function handles redirecting the user
  to their home page. */
 function goToLandingPage() {
-
     global $server_web_root;
-
     global $user;
-
     if ($user->getRole('SENX') == 'ADM') {
         header("Location: /" . $server_web_root . "/senior-experience/admin");
     } else {
@@ -31,6 +21,4 @@ function goToLandingPage() {
 }
 
 require_once(__DIR__ . "/../../shared/util/main.php");
-
 ?>
-
