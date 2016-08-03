3faaf9f66059c66eed5db8949f41154503c8cc93
bryres <bryres@bergen.org>
2016-08-03 07:44:42 -0400

    style fixes

--- career-day/admin/mentor/mentor_list.php
+++ career-day/admin/mentor/mentor_list.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 
     <!-- Styles -->
-    <link href="../../../shared/ss/main.css" rel="stylesheet" type="text/css" />
+    <link href="../../../shared/ss/main.css" rel="stylesheet" type="text/css"/>
     <link href="mentor_list.css" rel="stylesheet">
 </head>
 
@@ -24,34 +24,18 @@
 </header>
 
 <nav class="navbar">
-    <a href="#">
-        <div class="session-filter name">Name</div>
-    </a>
-    <a href="#">
-        <div class="session-filter company">Company</div>
-    </a>
-    <a href="#">
-        <div class="session-filter position">Position</div>
-    </a>
-    <a href="#">
-        <div class="session-filter teacher">Teacher</div>
-    </a>
-    <a href="#">
-        <div class="session-filter sessions">Sessions</div>
-    </a>
-    <a href="#">
-        <div class="session-filter room">Room</div>
-    </a>
-    <a href="#">
-        <div class="session-filter capacity">Max</div>
-    </a>
-    <a href="#">
-        <div class="session-filter delete"></div>
-    </a>
+    <div class="session-filter column name">Name</div>
+    <div class="session-filter column company">Company</div>
+    <div class="session-filter column position">Position</div>
+    <div class="session-filter column teacher">Teacher</div>
+    <div class="session-filter column sessions">Sessions</div>
+    <div class="session-filter column room">Room</div>
+    <div class="session-filter column capacity">Max</div>
+    <div class="session-filter delete"></div>
 </nav>
 
 
-<div class="list-container" style="max-width:90em;width:100%;">
+<div class="list-container">
 
     <?php foreach ($mentorList as $mentor) :
 
@@ -65,25 +49,29 @@
         $pres_max_capacity = $mentor['pres_max_capacity'];
         $sessions = $mentor['sessions'];
 
-    ?>
-        <div class="mentor" onclick="javascript:location.href='./index.php?mentor_id=<?php echo $mentor_id ?>&action=show_modify_mentor'">
-            <div class="session-filter name">&nbsp<?php echo($mentor_last_name . ', ' . $mentor_first_name); ?></div>
-            <div class="session-filter company">&nbsp<?php echo $mentor_company; ?></div>
-            <div class="session-filter position">&nbsp<?php echo $mentor_position; ?></div>
-            <div class="session-filter teacher">&nbsp<?php echo $pres_host_teacher; ?></div>
-            <div class="session-filter sessions">&nbsp<?php echo $sessions; ?></div>
-            <div class="session-filter room">&nbsp<?php echo $pres_room; ?></div>
-            <div class="session-filter capacity">&nbsp<?php echo $pres_max_capacity; ?></div>
-            <div class="session-filter delete"><img src="../../../shared/images/deleteIcon.gif" id="<?php echo $mentor_id ?>"/></div>
+        ?>
+        <div class="mentor"
+             onclick="javascript:location.href='./index.php?mentor_id=<?php echo $mentor_id ?>&action=show_modify_mentor'">
+            <div class="session-filter column name">
+                &nbsp<?php echo($mentor_last_name . ', ' . $mentor_first_name); ?></div>
+            <div class="session-filter column company">&nbsp<?php echo $mentor_company; ?></div>
+            <div class="session-filter column position">&nbsp<?php echo $mentor_position; ?></div>
+            <div class="session-filter column teacher">&nbsp<?php echo $pres_host_teacher; ?></div>
+            <div class="session-filter column sessions">&nbsp<?php echo $sessions; ?></div>
+            <div class="session-filter column room">&nbsp<?php echo $pres_room; ?></div>
+            <div class="session-filter column capacity">&nbsp<?php echo $pres_max_capacity; ?></div>
+            <div class="session-filter delete"><img src="../../../shared/images/deleteIcon.gif"
+                                                    id="<?php echo $mentor_id ?>"/></div>
         </div>
 
 
-     <?php endforeach; ?>
+    <?php endforeach; ?>
 </div>
 
 
 <div class="fab">
-    <a id="fab-action" trigger="./index.php?mentor_id=<?php echo $mentor_id?>&action=show_add_mentor"><span class="plus">+</span></a>
+    <a id="fab-action" trigger="./index.php?mentor_id=<?php echo $mentor_id ?>&action=show_add_mentor"><span
+            class="plus">+</span></a>
 </div>
 
 
@@ -103,7 +91,7 @@
         $.featherlight($('<iframe width="1000" height="800" src="' + $(this).attr('trigger') + '"/>'))
     });
 
-    $('img').click(function(){
+    $('img').click(function () {
         var id = $(this).attr('id');
         deleteMentor(id);
     });
