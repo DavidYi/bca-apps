001ac4d79ac59ed8a730e7007c0c239e8154e125
BrianMejia <BMejia0727@gmail.com>
2016-08-04 11:08:48 -0400

    styling

--- shared/room/room_modify.php
+++ shared/room/room_modify.php
@@ -26,7 +26,7 @@
 
                 <div id="button-div">
                     <button style="cursor: pointer" class="submit s" type="submit" name="choice" value="Modify">Submit</button>
-                    <button style="cursor: pointer" class="submit cancel" type="submit" name="choice" value="Back" formnovalidate>Cancel</button>
+                    <a href="index.php"><button style="cursor: pointer" class="submit cancel" type="button" name="choice" value="Back" formnovalidate>Cancel</button></a>
                 </div>
             </div>
         </div>
