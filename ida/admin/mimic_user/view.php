12cab1fa0565159f011074e50a7c183eaa8a0f4d
Katie <katkatz@bergen.org>
2016-09-07 09:41:26 -0400

    css errors fixed

--- ida/admin/mimic_user/view.php
+++ ida/admin/mimic_user/view.php
@@ -9,7 +9,7 @@
         <form action="." method="post">
             <input type="hidden" name="action" value="login">
 
-            <h1>Select User</h1>
+            <h1 class="title">Select User</h1>
             <select name="usr_id" id="usr_id">
                 <?php foreach ($user_list as $user) { ?>
                     <option value="<?php echo $user['usr_id']?>">
@@ -21,8 +21,10 @@
             </select>
             <br>
             <div id="padding"></div>
-            <button style="background-color: rgb(182, 226, 249); cursor: pointer" type="submit" value="Mimic User">Mimic User</button>
-            <a href="../index.php"><button style="background-color: rgb(247, 224, 97); cursor: pointer">Back</button></a>
+            <div id="button-div">
+                <button style="background-color: rgb(182, 226, 249); cursor: pointer" type="submit" value="Mimic User" id="submit">Mimic User</button>
+                <a href="../index.php"><button style="background-color: rgb(247, 224, 97); cursor: pointer">Back</button></a>
+            </div>
         </form>
     </div>
     </body>
