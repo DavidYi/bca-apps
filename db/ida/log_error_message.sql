8d83bbf6042c0a89feb6a42956e50d385077457e
bryres <bryres@bergen.org>
2016-10-11 10:50:44 -0400

    setup auto-enroll

--- db/ida/log_error_message.sql
+++ db/ida/log_error_message.sql
@@ -1,7 +1,7 @@
 drop procedure `log_error_message`;
-CREATE PROCEDURE `log_error_message`(IN `message` VARCHAR(1000), IN `method` INT(100))
+CREATE PROCEDURE `log_error_message`(IN `message` VARCHAR(1000), IN `method` VARCHAR(100))
     MODIFIES SQL DATA
 BEGIN
-	insert into log (log_lvl_cde, log_msg, log_src, log_mthd)
-	values ('ERR', message, 'STORED PROCEDURE', method);
+	insert into log (log_lvl_cde, log_msg, log_src, log_mthd, app_cde)
+	values ('ERR', message, 'STORED PROCEDURE', method, 'IDA');
 END
