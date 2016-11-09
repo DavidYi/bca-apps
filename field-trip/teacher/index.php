93cd163d69866c13fbba1a66f618d0d89eca34ca
Anoushka Ramkumar <anoushka@sooryen.com>
2016-11-09 10:50:06 -0500

    create trip page

--- field-trip/teacher/index.php
+++ field-trip/teacher/index.php
@@ -1,55 +1,6 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: macbook
- * Date: 1/5/16
- * Time: 2:04 PM
- */
+<?php 
+	require_once("../util/main.php");
 
-require_once(__DIR__ . "/../util/main.php");
-require_once(__DIR__ . "/../../shared/model/user_db.php");
-require_once(__DIR__ . "/../model/teacher_db.php");
-require_once(__DIR__ . "/../model/times_db.php");
-
-verify_teacher();
-
-$action = strtolower(filter_input(INPUT_POST, 'action'));
-if ($action == NULL) {
-    $action = strtolower(filter_input(INPUT_GET, 'action'));
-    if ($action == NULL) {
-        $action = 'list_options';
-    }
-}
-
-$usr_id = $user->usr_id;
-$time_strings = get_time_strings($usr_id);
-
-switch($action){
-    case "logout":
-        if (isset($action) and ($action == "logout")) {
-            if (isset($_SESSION['prev_usr_id'])) {
-                $_SESSION['user'] = User::getUserByUsrId($_SESSION['prev_usr_id']);
-                $_SESSION['prev_usr_id'] = NULL;
-                header("Location: ../admin/index.php");
-            } else {
-                session_destroy();
-                header("Location: ../index.php");
-            }
-        }
-        break;
-    case "delete_course":
-        
-        delete_course($_GET['course_id']);
-        $courses = get_course_by_user($user->usr_id);
-        include("./view.php");
-        break;
-
-    default:
-        $courses = get_course_by_user($user->usr_id);
-        $usr_id = $user->usr_id;
-        $available_times = get_times($usr_id);
-        include("./view.php");
-        break;
-}
-
+    include("view.php");
+?>
 
--- field-trip/teacher/teacher_add_course/view.php
+++ field-trip/teacher/teacher_add_course/view.php
@@ -1,20 +1,12 @@
 <!doctype html>
 <html>
     <head>
+        <link href="https://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
         <link href="view.css" rel="stylesheet" type="text/css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-          
-        <script>
-        $('.datepicker').pickadate({
-            selectMonths: true, // Creates a dropdown to control month
-            selectYears: 15 // Creates a dropdown of 15 years to control year
-          });
-        </script>
-
     </head>
     <body>
         <form action="index.php" method="post">
@@ -23,25 +15,25 @@
             <div id="box">
                 <p class="title">Create Trip</p>
 
-                <label class="spacing">
-                    <span>Trip Name</span>
+                <div class = "trip_input">
+                    <label class="spacing" for= "trip_name">Trip Name</label>
                     <input type="text" name="trip_name" required>
-                </label>
+                </div>
 
-                <label class="spacing">
-                    <span>Start Date</span>
-                    <input type="date" class="datepicker">
-                </label>
+                <div class = "trip_input">
+                    <label class="spacing" for= "start_date">Start Date</label>
+                    <input type="date" name="start_date" class="datepicker">
+                </div>
 
-                <label class="spacing">
-                    <span>Trip Name</span>
-                    <input type="text" name="trip_name" required>
-                </label>
+                <div class = "trip_input">
+                    <label class="spacing" for= "end_date">End Date</label>
+                    <input type="date" name="end_date" class="datepicker">
+                </div>
 
-                <label class="spacing">
-                    <span>Purpose</span>
-                    <textarea name="trip_purpose" required></textarea>
-                </label>
+               <div class = "trip_input">
+                    <label class="spacing" for = "purpose">Purpose of Trip</label>
+                    <textarea type="textarea" name="purpose" required></textarea>
+                </div>
 
                 <div class="button_wrapper">
                     <button class="submit back" type="button" onclick="location.href='../index.php'">Back</button>
@@ -51,4 +43,13 @@
         </form>
 
   </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+   <script>
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+          });
+        </script>
 </html>
