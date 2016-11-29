e0650aa84c61de41a99d125695f5f988d80b36ab
bryres <bryres@bergen.org>
2016-11-29 11:19:54 -0500

    revert to prior version.  file was corrupted by work on field trip app.

--- teacher-proctoring/admin/add/view.php
+++ teacher-proctoring/admin/add/view.php
@@ -1,55 +1,140 @@
-<!doctype html>
 <html>
-    <head>
-        <link href="https://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet">
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
-        <link href="view.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <form action="index.php" method="post">
-            <input type="hidden" name="action" value="add_course">
 
-            <div id="box">
-                <p class="title">Create Trip</p>
+<head>
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link href="../../../shared/ss/main.css" rel="stylesheet">
+    <link href="view.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script src="jquery.timepicker.js"></script>
+    <script src="functions.js"></script>
+    <script src="semantic/dist/semantic.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#basicExample').timepicker();
 
-                <div class = "trip_input">
-                    <label class="spacing" for= "trip_name">Trip Name</label>
-                    <input type="text" name="trip_name" required>
+            $('.ui.dropdown').selectmenu();
+        });
+    </script>
+    <title>Admin: Add Test</title>
+</head>
+
+<body>
+<header>
+    <h1 style="margin:0;"class="title">Add Test</h1>
+</header>
+<div id="box">
+    <div id="wrapper">
+        <div id ="columns">
+            <form action="." method="post" id="inputs">
+                <input type="hidden" name="action" value="add_test">
+
+                <input class="mod" name="test_name" placeholder="Test Name" required><BR>
+
+                <input class="mod" name="date" placeholder="Date" type="text" id="datepicker" required><BR>
+
+                <select name="test_cde" class="mod" required>
+                    <i class="dropdown icon"></i>
+                    <option value="" disabled>Test Type</option>
+                    <?php foreach ($testTypes as $test) { ?>
+                        <?php if ($user->getRole("TPOR") == $test['test_type_cde'] || $user->getRole("TPOR") == "ADM") { ?>
+                            <option value="<?php echo $test['test_type_cde']?>">
+                                <?php echo $test['test_type_desc']?>
+                            </option>
+                        <?php }
+                    } ?>
+                </select><BR>
+
+                <!-- Test -->
+
+                <select name="room_id" class="mod" required>
+                    <i class="dropdown icon"></i>
+                    <option value="" disabled>Room Number</option>
+                    <?php
+                    foreach ($rooms as $room) { ?>
+                        <option value="<?php echo $room['rm_id']; ?>"><?php echo $room['rm_nbr']?></option>
+                    <?php } ?>
+                </select><BR>
+
+                <!-- Time -->
+
+                <hr>
+
+                <p class="proctortitle">Proctors Needed</p>
+
+                <table class="tg">
+                    <tr>
+                        <td class="tg-yw41">
+                            <div class="ui labeled input">
+                                <div class="ui label">01 - 03</div>
+                                <input name="one_three" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                        <td class="tg-yw4l">
+                            <div class="ui labeled input center">
+                                <div class="ui label">04 - 06</div>
+                                <input name="four_six" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                        <td class="tg-yw4l">
+                            <div class="ui labeled input right">
+                                <div class="ui label">07 - 09</div>
+                                <input name="seven_nine" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="tg-031e">
+                            <div class="ui labeled input">
+                                <div class="ui label">10 - 12</div>
+                                <input name="ten_twelve" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                        <td class="tg-yw4l">
+                            <div class="ui labeled input center">
+                                <div class="ui label">13 - 15</div>
+                                <input name="thirteen_fifteen" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                        <td class="tg-yw4l">
+                            <div class="ui labeled input right">
+                                <div class="ui label">16 - 18</div>
+                                <input name="sixteen_eighteen" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="tg-yw4l">
+                            <div class="ui labeled input">
+                                <div class="ui label">19 - 21</div>
+                                <input name="nineteen_twentyone" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                        <td class="tg-yw4l">
+                            <div class="ui labeled input center">
+                                <div class="ui label">22 - 24</div>
+                                <input name="twentytwo_twentyfour" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                        <td class="tg-yw4l">
+                            <div class="ui labeled input right">
+                                <div class="ui label">25 - 27</div>
+                                <input name="twentyfive_twentyseven" type="text" class="modinput" value="0">
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+
+                <div id="button-div">
+                    <button class="submit s" type="submit" name="choice" value="Add">Add</button>
+                    <button class="submit cancel" type="submit"
+                            name="choice" value="Return" formnovalidate>Cancel</button>
                 </div>
-
-                <div class = "trip_input">
-                    <label class="spacing" for= "start_date">Start Date</label>
-                    <input type="date" name="start_date" class="datepicker">
-                </div>
-
-                <div class = "trip_input">
-                    <label class="spacing" for= "end_date">End Date</label>
-                    <input type="date" name="end_date" class="datepicker">
-                </div>
-
-               <div class = "trip_input">
-                    <label class="spacing" for = "purpose">Purpose of Trip</label>
-                    <textarea type="textarea" name="purpose" required></textarea>
-                </div>
-
-                <div class="button_wrapper">
-                    <button class="submit back" type="button" onclick="location.href='../index.php'">Back</button>
-                    <button class="submit s" type="submit" name="choice" value="Add Course">Submit</button>
-                </div>
-            </div>
-        </form>
-
-  </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-   <script>
-        $('.datepicker').pickadate({
-            selectMonths: true, // Creates a dropdown to control month
-            selectYears: 15 // Creates a dropdown of 15 years to control year
-          });
-        </script>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
 </html>
